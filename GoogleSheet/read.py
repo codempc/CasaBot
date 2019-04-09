446ab5dfcd7a051eb5a9ae936beadf81b7708b62
Jonathan Tjeng <jonathanreyhanari@yahoo.com>
2019-04-09 13:27:57 +1000

    add view_all_data func

--- GoogleSheet/read.py
+++ GoogleSheet/read.py
@@ -1,4 +1,8 @@
 # This is where we read data from googlesheet and then use that data for webhook.
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pprint
+
 def get_rate(bank_name, amount, time):
     # TODO: Get data from GoogleSheets.
     rate = "4.8%"  # Get from googlesheets based on the condition (bank_name, amount, time)
@@ -9,4 +13,24 @@ def get_rate(bank_name, amount, time):
     if time == "":
         time = "30 years"  # Get data from googlesheets first row for time
 
-    return "The rate for " + bank_name + " with a loan amount of " + amount + " and year period of " + time + " is " + rate
+    print(type(bank_name))
+    print(type(amount))
+    print(type(time))
+    print(type(rate))
+
+    # TODO: Randomise words, not only using one.
+    return "The rate for " + bank_name + " with a loan amount of " + "$" + str(amount) + " and period of " + \
+        str(time) + " years is " + rate
+
+
+def view_all_data(file_name):
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('bank_secret.json', scope)
+    client = gspread.authorize(creds)
+
+    sheet = client.open(file_name).sheet1
+
+    pp = pprint.PrettyPrinter()
+    result = sheet.get_all_records()
+    pp.pprint(result)
+    return result
