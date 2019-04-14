b3d4ed050d8dddf922abe46907a8ffa0eef45536
rico <limyandi@gmail.com>
2019-04-14 15:36:27 +1000

    change files

--- GoogleSheet/read.py
+++ GoogleSheet/read.py
@@ -3,31 +3,28 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pprint
 
+
 def get_rate(bank_name, amount, time):
     # TODO: Get data from GoogleSheets.
     rate = "4.8%"  # Get from googlesheets based on the condition (bank_name, amount, time)
 
     # IF no loan amount and time, just give the first row while also giving the details. i.e.
     if amount == "":
-        amount = "$40000"  # Get data from googlesheets first row for amount
+        amount = "40000"  # Get data from googlesheets first row for amount
     if time == "":
-        time = "30 years"  # Get data from googlesheets first row for time
-
-    print(type(bank_name))
-    print(type(amount))
-    print(type(time))
-    print(type(rate))
+        time = "30"  # Get data from googlesheets first row for time
 
     # TODO: Randomise words, not only using one.
     return "The rate for " + bank_name + " with a loan amount of " + "$" + str(amount) + " and period of " + \
         str(time) + " years is " + rate
+
 
 def view_all_data(file_name):
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('bank_secret.json', scope)
     client = gspread.authorize(creds)
 
-    sheet = client.open(file_name).sheet1
+    sheet = client.open(file_name.lower()).sheet1
 
     pp = pprint.PrettyPrinter()
     result = sheet.get_all_records()
