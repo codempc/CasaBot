9a90f7f59867ce91e39bc475ddec1f14d8370f30
rico <limyandi@gmail.com>
2019-05-05 19:50:04 +1000

    clean

--- GoogleSheet/read.py
+++ GoogleSheet/read.py
@@ -27,7 +27,9 @@ def get_rate(bank_name, amount, time):
     #     amount = "40000"  # Get data from googlesheets first row for amount
     # if time == "":
     #     time = "30"  # Get data from googlesheets first row for time
+    # TODO: Get maximum and minimum loan amount for a bank.
 
+    # TODO: Fixed the required parameters for show rate.
     if time['unit'] == "mo":
         if time['amount'] > 48:
             time['amount'] = format(time['amount'] / 12.0, '.1f')
@@ -37,7 +39,6 @@ def get_rate(bank_name, amount, time):
     else:
         time_unit = 'year' if time['amount'] == 1 else "years"
 
-    # TODO: Randomise words, not only using one.
     output_string = random.choice(SHOW_RATE_RESPONSE)
     response = output_string.format(
         bank_name=bank_name,
@@ -51,7 +52,7 @@ def get_rate(bank_name, amount, time):
 
 
 def get_best_rate(bank_name="", amount="", time="", mortage_types=""):
-    # TODO: Get the list of arrays from the read function from GoogleSheets.
+    # TODO: Get the list of arrays from the get_lowest_bank function from GoogleSheets.
     # TODO: Format data for the result should be in array like below
     #  (although in the real situation we should show more columns).
     result = [['CommBank', '3.2%'], ['Westpac', '3.33%'],
@@ -64,7 +65,6 @@ def get_best_rate(bank_name="", amount="", time="", mortage_types=""):
 
 def view_all_data(file_name):
     sheet = client.open(file_name.lower()).sheet1
-    # TODO: Get maximum and minimum loan amount for a bank.
     # TODO: Get 5 with the highest rate (no condition).
     # TODO: Get 5 with the highest rate for different bank
     # TODO: Get 5 with the highest rate for different amount of money.
@@ -141,6 +141,6 @@ def get_lowest_bank(file_name, bank_name=None, mortgage=None, yearFixed=None):
     print(top_lowest)
     return top_lowest
 
-
+# Example:
 get_lowest_bank('casa_bank', bank_name='CommBank (CM)',
                 mortgage='IO', yearFixed=1)
